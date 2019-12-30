dc9d73585a3edd2fd87028230953137ba9651c42
Yudi Mufti F <yudimufti@gmail.com>
2019-12-30 15:39:14 +0700

    First Commit

--- app/src/main/java/id/yudimf/moviecatalogue2/alarm/NotificationItem.kt
+++ app/src/main/java/id/yudimf/moviecatalogue2/alarm/NotificationItem.kt
@@ -3,6 +3,5 @@ package id.yudimf.moviecatalogue2.alarm
 data class NotificationItem(
     var id: Int,
     var sender: String?,
-    var message: String?,
-    var test:Int
+    var message: String?
 )
